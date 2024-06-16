9a329887179c1c6e68198a94c77c69318b90cb59
muhammad-jiku <muhammadjiku364@gmail.com>
2024-06-16 15:14:49 +0600

    users controller func added

---
+++ src/app/modules/users/users.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express'
+import usersService from './users.services'
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const { user } = req.body
+    const result = await usersService.createUser(user)
+    res.status(200).json({
+      success: true,
+      message: 'user created successfully!',
+      data: result,
+    })
+  } catch (err) {
+    if (err) {
+      console.log(err)
+    }
+    res.status(400).json({
+      sucess: false,
+      message: 'Failed to create user',
+    })
+  }
+}
+
+export default {
+  createUser,
+}
