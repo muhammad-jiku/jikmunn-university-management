c18f6b05c24f445023c70f4659bc9bb4954dc73a
muhammad-jiku <muhammadjiku364@gmail.com>
2024-06-16 15:33:31 +0600

    users route init

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.routes'
 const app: Application = express()
 
 app.use(cors())
 
-//parser
+// parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-//Testing
+// Application routes
+app.use('/api/v1/users/', usersRouter)
+
+// Testing
 app.get('/', (req: Request, res: Response) => {
   res.send({
     message: 'Server is working successfully!!',
