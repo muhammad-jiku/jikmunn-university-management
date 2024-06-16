77ca2c8c7478fc74de5f2bf1d17b27b8b0c5aabd
muhammad-jiku <muhammadjiku364@gmail.com>
2024-06-16 12:08:43 +0600

    users interface added

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,5 @@ dotenv.config({ path: path.join(process.cwd(), '.env') })
 export default {
   port: process.env.PORT,
   db_uri: process.env.DB_URI,
+  default_user_pass: process.env.DEFAULT_USER_PASS,
 }
